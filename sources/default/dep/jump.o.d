2c6e479bfa3adbbbcdd1085e404bed922bc45492
C00206286 <C00206286@itcarlow.ie>
2019-01-26 15:46:19 +0000

    banana

--- sources/default/dep/jump.o.d
+++ sources/default/dep/jump.o.d
@@ -1,29 +1,36 @@
 jump.o: ../jump.c /usr/lib/gcc/avr/5.4.0/include/stdbool.h \
- /usr/lib/avr/include/avr/io.h /usr/lib/avr/include/avr/sfr_defs.h \
- /usr/lib/avr/include/inttypes.h /usr/lib/gcc/avr/5.4.0/include/stdint.h \
- /usr/lib/avr/include/stdint.h /usr/lib/avr/include/avr/iom644.h \
+ /usr/lib/avr/include/stdio.h /usr/lib/avr/include/inttypes.h \
+ /usr/lib/gcc/avr/5.4.0/include/stdint.h /usr/lib/avr/include/stdint.h \
+ /usr/lib/gcc/avr/5.4.0/include/stdarg.h \
+ /usr/lib/gcc/avr/5.4.0/include/stddef.h /usr/lib/avr/include/avr/io.h \
+ /usr/lib/avr/include/avr/sfr_defs.h /usr/lib/avr/include/avr/iom644.h \
  /usr/lib/avr/include/avr/iomxx4.h /usr/lib/avr/include/avr/portpins.h \
  /usr/lib/avr/include/avr/common.h /usr/lib/avr/include/avr/version.h \
  /usr/lib/avr/include/avr/fuse.h /usr/lib/avr/include/avr/lock.h \
- /usr/lib/avr/include/stdlib.h /usr/lib/gcc/avr/5.4.0/include/stddef.h \
- /usr/lib/avr/include/avr/pgmspace.h ../../../kernel/uzebox.h \
- ../../../kernel/defines.h ../../../kernel/videoMode3/videoMode3.def.h \
- ../../../kernel/kernel.h ../../../kernel/videoMode3/videoMode3.h \
- /usr/lib/avr/include/math.h /usr/lib/avr/include/avr/interrupt.h \
- ../data/tiles.inc ../data/sprites.inc ../data/patches.inc \
- ../data/music.inc
+ /usr/lib/avr/include/stdlib.h /usr/lib/avr/include/avr/pgmspace.h \
+ ../../../kernel/uzebox.h ../../../kernel/defines.h \
+ ../../../kernel/videoMode3/videoMode3.def.h ../../../kernel/kernel.h \
+ ../../../kernel/videoMode3/videoMode3.h /usr/lib/avr/include/math.h \
+ /usr/lib/avr/include/avr/interrupt.h ../data/tiles.inc \
+ ../data/sprites.inc ../data/patches.inc ../data/music.inc
 
 /usr/lib/gcc/avr/5.4.0/include/stdbool.h:
 
-/usr/lib/avr/include/avr/io.h:
-
-/usr/lib/avr/include/avr/sfr_defs.h:
+/usr/lib/avr/include/stdio.h:
 
 /usr/lib/avr/include/inttypes.h:
 
 /usr/lib/gcc/avr/5.4.0/include/stdint.h:
 
 /usr/lib/avr/include/stdint.h:
+
+/usr/lib/gcc/avr/5.4.0/include/stdarg.h:
+
+/usr/lib/gcc/avr/5.4.0/include/stddef.h:
+
+/usr/lib/avr/include/avr/io.h:
+
+/usr/lib/avr/include/avr/sfr_defs.h:
 
 /usr/lib/avr/include/avr/iom644.h:
 
@@ -40,8 +47,6 @@ jump.o: ../jump.c /usr/lib/gcc/avr/5.4.0/include/stdbool.h \
 /usr/lib/avr/include/avr/lock.h:
 
 /usr/lib/avr/include/stdlib.h:
-
-/usr/lib/gcc/avr/5.4.0/include/stddef.h:
 
 /usr/lib/avr/include/avr/pgmspace.h:
 
